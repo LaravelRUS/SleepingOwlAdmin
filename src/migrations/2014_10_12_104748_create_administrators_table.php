8c0ad0c937a775dd61cb510ef5ff4c04a418cd35
Sleeping Owl <owl.sleeping@yahoo.com>
2014-10-29 14:51:41 +0400

    Administrator username maximum length reduced to 190 to support utf8mb4 charset. Fix #1

--- src/migrations/2014_10_12_104748_create_administrators_table.php
+++ src/migrations/2014_10_12_104748_create_administrators_table.php
@@ -15,7 +15,7 @@ class CreateAdministratorsTable extends Migration {
 		Schema::create('administrators', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('username')->unique();
+			$table->string('username', 190)->unique();
 			$table->string('password', 60);
 			$table->string('name');
 			$table->string('remember_token', 100)->nullable();
