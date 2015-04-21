be6acc64cf860d62ca1c16aabebd71c6671dd56c
Sleeping Owl <owl.sleeping@yahoo.com>
2015-04-21 18:47:03 +0400

    Multiselect form item

--- src/SleepingOwl/Admin/FormItems/MultiSelect.php
+++ src/SleepingOwl/Admin/FormItems/MultiSelect.php
@@ -1,11 +1,20 @@
 <?php namespace SleepingOwl\Admin\FormItems;
 
-use SleepingOwl\Admin\AssetManager\AssetManager;
-use SleepingOwl\Admin\Repository\BaseRepository;
+use Illuminate\Database\Eloquent\Collection;
 
 class MultiSelect extends Select
 {
 
 	protected $view = 'multiselect';
+
+	public function value()
+	{
+		$value = parent::value();
+		if ($value instanceof Collection)
+		{
+			return $value->lists('id');
+		}
+		return $value;
+	}
 
 }
