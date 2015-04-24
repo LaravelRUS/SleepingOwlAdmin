4c27cf77cc8ecb394eb2ef8e69c0748e14ec2dd0
Sleeping Owl <owl.sleeping@yahoo.com>
2015-04-24 20:05:21 +0400

    Tab content offset fixed

--- src/views/default/display/tree.blade.php
+++ src/views/default/display/tree.blade.php
@@ -1,5 +1,5 @@
 @if ($creatable)
-	<a class="btn btn-primary navbar-btn" href="{{ $createUrl }}"><i class="fa fa-plus"></i> {{ trans('admin::lang.table.new-entry') }}</a>
+	<a class="btn btn-primary" href="{{ $createUrl }}"><i class="fa fa-plus"></i> {{ trans('admin::lang.table.new-entry') }}</a>
 @endif
 <div class="dd nestable" data-url="{{ $url }}/reorder">
 	<ol class="dd-list">
