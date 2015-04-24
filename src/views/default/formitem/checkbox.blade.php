15d02d1ec818b4a58d2b0ff75b8a3a8bab19c05d
Sleeping Owl <owl.sleeping@yahoo.com>
2015-04-24 18:33:46 +0400

    Checkbox fix

--- src/views/default/formitem/checkbox.blade.php
+++ src/views/default/formitem/checkbox.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group {{ $errors->has($name) ? 'has-error' : '' }}">
 	<div class="checkbox">
 		<label>
-			<input type="checkbox" name="{{ $name }}" value="1" />{{ $label }}
+			<input type="checkbox" name="{{ $name }}" value="1" {!! $value ? 'checked="checked"' : '' !!} />{{ $label }}
 		</label>
 	</div>
 	@include(AdminTemplate::view('formitem.errors'))
