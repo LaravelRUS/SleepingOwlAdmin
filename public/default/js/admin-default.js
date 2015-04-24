abdb3bc5d779576776bc969d56f416046b1d1574
Sleeping Owl <owl.sleeping@yahoo.com>
2015-04-24 19:09:19 +0400

    Menu item highlight fix 3

--- public/default/js/admin-default.js
+++ public/default/js/admin-default.js
@@ -8,10 +8,10 @@ $(function ()
 		currentPage = currentPage.replace(/\/create$/, '');
 		currentPage = currentPage.replace(/\/([0-9]+)\/edit/, '');
 
-		var currentPageLink = $('#side-menu a[href="' + currentPage + '"]');
+		var currentPageLink = $('#side-menu a[href="' + window.location.href + '"]');
 		if ( ! currentPageLink.length)
 		{
-			currentPageLink = $('#side-menu a[href="' + window.location.href + '"]');
+			currentPageLink = $('#side-menu a[href="' + currentPage + '"]');
 		}
 		currentPageLink.addClass('active').parents('li').addClass('active').end().parents('ul').addClass('collapse').addClass('in');
 	})();
