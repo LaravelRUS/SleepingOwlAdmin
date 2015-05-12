775cebe0c583aae890d5fc235407d64b6f5fd737
Sleeping Owl <owl.sleeping@yahoo.com>
2015-05-12 15:05:58 +0400

    Session fix

--- src/SleepingOwl/Admin/Helpers/StartSession.php
+++ src/SleepingOwl/Admin/Helpers/StartSession.php
@@ -15,63 +15,14 @@ class StartSession
 	function __construct(Encrypter $encrypter, Request $request, SessionManager $manager)
 	{
 		$this->encrypter = $encrypter;
-		$this->request = clone $request;
+		$this->request = $request;
 		$this->manager = $manager;
 	}
 
 	public function run()
 	{
-		$this->decrypt($this->request);
 		$session = $this->startSession($this->request);
 		$this->request->setSession($session);
-	}
-
-	protected function decrypt(Request $request)
-	{
-		foreach ($request->cookies as $key => $c)
-		{
-			try
-			{
-				$request->cookies->set($key, $this->decryptCookie($c));
-			}
-			catch (DecryptException $e)
-			{
-				$request->cookies->set($key, null);
-			}
-		}
-
-		return $request;
-	}
-
-	/**
-	 * Decrypt the given cookie and return the value.
-	 *
-	 * @param  string|array  $cookie
-	 * @return string|array
-	 */
-	protected function decryptCookie($cookie)
-	{
-		return is_array($cookie)
-			? $this->decryptArray($cookie)
-			: $this->encrypter->decrypt($cookie);
-	}
-
-	/**
-	 * Decrypt an array based cookie.
-	 *
-	 * @param  array  $cookie
-	 * @return array
-	 */
-	protected function decryptArray(array $cookie)
-	{
-		$decrypted = [];
-
-		foreach ($cookie as $key => $value)
-		{
-			$decrypted[$key] = $this->encrypter->decrypt($value);
-		}
-
-		return $decrypted;
 	}
 
 	protected function startSession(Request $request)
@@ -87,7 +38,7 @@ class StartSession
 	{
 		$session = $this->manager->driver();
 
-		$session->setId($request->cookies->get($session->getName()));
+		$session->setId($this->encrypter->decrypt($request->cookies->get($session->getName())));
 
 		return $session;
 	}
