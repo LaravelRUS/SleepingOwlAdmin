d9840a9887e37a35ba44fde5a59d48b32f5eb572
Butschster <butschster@gmail.com>
2016-02-22 01:44:32 +0300

    Fix navigation html code
    Add @yled sections 'sidebar.top', 'sidebar.bottom', 'sodebar.ul.top', 'sidebar.ul.bottom' to inject custom html code to sidebar

--- resources/views/default/_partials/navigation.blade.php
+++ resources/views/default/_partials/navigation.blade.php
@@ -1,18 +1,30 @@
 <div class="navbar-default sidebar" role="navigation">
+
+	@yield('sidebar.top')
+
 	<div class="sidebar-nav navbar-collapse">
 		<ul class="nav" id="side-menu">
-		@foreach(AdminNavigation::getRootSection()->getPages() as $item)
+
+			@yield('sidebar.ul.top')
+
+			@foreach(AdminNavigation::getRootSection()->getPages() as $item)
 			<li @if($item->isActive())class="active"@endif>
 				<a href="{{ $item->getUrl() }}">
 					{!! $item->getIcon() !!}
 					<span class="mm-text">{!! $item->getName() !!}</span>
 				</a>
 			</li>
-		@endforeach
+			@endforeach
 
-		@foreach(AdminNavigation::getRootSection()->getSections() as $section)
-			@include(AdminTemplate::getTemplateViewPath('_partials.navigation.sections', ['section' => $section]))
-		@endforeach
+			@foreach(AdminNavigation::getRootSection()->getSections() as $section)
+				@include(AdminTemplate::getTemplateViewPath('_partials.navigation.sections'), ['section' => $section])
+			@endforeach
+
+			@yield('sidebar.ul.bottom')
+
 		</ul>
+
+		@yield('sidebar.bottom')
+
 	</div>
 </div>
--- resources/views/default/_partials/navigation/sections.blade.php
+++ resources/views/default/_partials/navigation/sections.blade.php
@@ -1,22 +1,23 @@
 @if(count($section) > 0 or $section->hasSections())
-    <li class="mm-dropdown @if($section->isActive()) open @endif">
+    <li>
         <a href="#">
             {!! $section->getIcon() !!}
-            <span class="mm-text">{!! $section->getName() !!}</span>
+            {!! $section->getName() !!}
+            <span class="fa arrow"></span>
         </a>
-        <ul>
+        <ul class="nav nav-second-level">
             @foreach($section as $item)
                 <?php if($item->isHidden()) continue; ?>
                 <li @if ($item->isActive())class="active"@endif>
                     <a href="{{ $item->getUrl() }}">
                         {!! $item->getIcon() !!}
-                        <span class="mm-text">{!! $item->getName() !!}</span>
+                        {!! $item->getName() !!}
                     </a>
                 </li>
             @endforeach
 
             @foreach($section->getSections() as $sub_section )
-            @include(AdminTemplate::getTemplateViewPath('_partials.navigation.sections', ['section' => $sub_section])
+            @include(AdminTemplate::getTemplateViewPath('_partials.navigation.sections'), ['section' => $sub_section])
             @endforeach
         </ul>
     </li>
