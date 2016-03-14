235e292a64ba82241f7ac164fa24796a7e062fb0
butschster <butschster@gmail.com>
2016-03-14 18:06:10 +0300

    issue #34
    By default password field required to fill if model not exists. If you want to change it, you can fill password field otherwise leave field empty.
    
    Or set  `AdminFormElement::password('password', 'Password')->allowEmpty()` to allow check password validation rules by each model updating

--- src/Form/Element/Password.php
+++ src/Form/Element/Password.php
@@ -4,4 +4,57 @@ namespace SleepingOwl\Admin\Form\Element;
 
 class Password extends NamedFormElement
 {
+    /**
+     * @var bool
+     */
+    protected $allowEmpty = false;
+
+    public function save()
+    {
+        $value = $this->getValue();
+
+        if (! $this->isAllowedEmptyValue() and $this->getModel()->exists() and empty($value)) {
+            return;
+        }
+
+        parent::save();
+    }
+
+    /**
+     * @return array
+     */
+    public function getValidationRules()
+    {
+        $data = parent::getValidationRules();
+
+        if (! $this->isAllowedEmptyValue() and $this->getModel()->exists()) {
+            foreach ($data as $field => $rules) {
+                foreach ($rules as $i => $rule) {
+                    if ($rule == 'required') {
+                        unset($data[$field][$i]);
+                    }
+                }
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isAllowedEmptyValue()
+    {
+        return $this->allowEmpty;
+    }
+
+    /**
+     * @return $this
+     */
+    public function allowEmptyValue()
+    {
+        $this->allowEmpty = true;
+
+        return $this;
+    }
 }
