3a7c5789e53a45f5a69c45d8c7355ace6bada7a0
butschster <butschster@gmail.com>
2016-05-06 12:20:42 +0300

    fix #106

--- resources/views/default/form/default.blade.php
+++ resources/views/default/form/default.blade.php
@@ -3,7 +3,7 @@
 	<input type="hidden" name="_redirectBack" value="{{ $backUrl }}" />
 	<input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
-	@include(AdminTemplate::getViewPath('form.partials.elements'), ['items' => $formItems])
+	@include(AdminTemplate::getViewPath('form.partials.elements'), ['items' => $items])
 
 	{!! $buttons->render() !!}
 </form>
