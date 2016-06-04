8de8bfcf909290b3deb88bcea9adcc3fc27e20ee
butschster <butschster@gmail.com>
2016-06-04 04:12:00 -0400

    Applied fixes from StyleCI

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -426,7 +426,7 @@ class AdminController extends Controller
      */
     protected function registerBreadcrumb($title, $parent)
     {
-        Breadcrumbs::register('render', function ($breadcrumbs) use ($title,$parent) {
+        Breadcrumbs::register('render', function ($breadcrumbs) use ($title, $parent) {
             $breadcrumbs->parent($parent);
             $breadcrumbs->push($title);
         });
