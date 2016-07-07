962e5b1e55c86be258806c618d3e4c121355cc41
Butschster <butschster@gmail.com>
2016-07-08 00:42:21 +0300

    fix

--- resources/views/default/column/url.blade.php
+++ resources/views/default/column/url.blade.php
@@ -1,4 +1,4 @@
-@if ( ! is_null($url))
+@if (!empty($url))
 <a href="{{ $value }}" {{ app('html')->attributes($linkAttributes) }}>
 	<i class="fa fa-arrow-circle-o-right" data-toggle="tooltip" title="{{ trans('sleeping_owl::lang.table.filter-goto') }}"></i>
 </a>
