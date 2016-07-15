f2add75e99f049df1ed193842d903681ab10ae55
butschster <butschster@gmail.com>
2016-07-15 16:10:50 +0300

    Fix tabbed form html

--- resources/views/default/form/tabbed.blade.php
+++ resources/views/default/form/tabbed.blade.php
@@ -3,7 +3,7 @@
 	<input type="hidden" name="_redirectBack" value="{{ $backUrl }}" />
 	<input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
-	<div role="tabpanel">
+	<div class="panel-heading nav-tabs-custom" role="tabpanel" style="padding-bottom: 0">
 		<ul class="nav nav-tabs" role="tablist">
 			<?php $active = null; ?>
 			@foreach ($items as $label => $_tmp)
@@ -12,10 +12,13 @@
 						$active = $label;
 					}
 				?>
-				<li role="presentation" {!! ($active == $label) ? 'class="active"' : '' !!}><a href="#{{ md5($label) }}" aria-controls="{{ md5($label) }}" role="tab" data-toggle="tab">{{ $label }}</a></li>
+				<li role="presentation" {!! ($active == $label) ? 'class="active"' : '' !!}>
+					<a href="#{{ md5($label) }}" aria-controls="{{ md5($label) }}" role="tab" data-toggle="tab">{{ $label }}</a>
+				</li>
 			@endforeach
 		</ul>
-
+	</div>
+	<div class="panel-body">
 		<div class="tab-content">
 			@foreach ($items as $label => $formItems)
 				<div role="tabpanel" class="tab-pane {!! ($active == $label) ? 'in active' : '' !!}" id="{{ md5($label) }}">
--- src/Form/FormTabbed.php
+++ src/Form/FormTabbed.php
@@ -8,4 +8,16 @@ class FormTabbed extends FormDefault
      * @var string
      */
     protected $view = 'form.tabbed';
+
+    /**
+     * Initialize form.
+     */
+    public function initialize()
+    {
+        $this->getButtons()->setHtmlAttribute('class', 'panel-footer');
+
+        $this->setHtmlAttribute('class', 'panel panel-default');
+
+        parent::initialize();
+    }
 }
