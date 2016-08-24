73c7da6bac681d5b6dff83ee1646e05db789e8ee
butschster <butschster@gmail.com>
2016-08-24 09:39:25 +0300

    fixed bug with using several wysiwyg filelds

--- resources/views/default/form/element/wysiwyg.blade.php
+++ resources/views/default/form/element/wysiwyg.blade.php
@@ -1,6 +1,6 @@
 @push('footer-scripts')
 <script>
-    Admin.Modules.add('wysiwyg', function() {
+    Admin.Modules.add('wysiwyg.{{ $name }}', function() {
         window.Admin.WYSIWYG.switchOn('{{ $name }}', '{{ $editor }}', {!! $parameters !!})
     })
 </script>
