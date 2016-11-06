8d4172257876de4a0b0ebb6cea6938e72f8393ab
Joey <joey.xf@gmail.com>
2016-11-06 17:40:26 +0800

    fix AdminColumnEditable ajax url error

--- resources/views/default/column/editable/checkbox.blade.php
+++ resources/views/default/column/editable/checkbox.blade.php
@@ -3,7 +3,7 @@
        class="inline-editable"
        data-name="{{ $name }}"
        data-value="{{ $value }}"
-       data-url="{{ request()->url() }}"
+       data-url="{{ strstr(request()->url(), 'async', true) }}"
        data-type="checklist"
        data-pk="{{ $id }}"
        data-source="{ 1 : '{{ $checkedLabel }}' }"
