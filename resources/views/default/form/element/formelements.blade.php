acd154ee711050e324a56eab3ab88c328cf312bc
butschster <butschster@gmail.com>
2016-12-19 10:03:53 +0300

    Update formelements.blade.php

--- resources/views/default/form/element/formelements.blade.php
+++ resources/views/default/form/element/formelements.blade.php
@@ -1,15 +1,11 @@
 @if($items instanceof \SleepingOwl\Admin\Form\Element\Columns)
-    <div class="form-elements">
     {!! $items->render() !!}
-    </div>
 @else
-    <div class="form-elements">
-        @foreach ($items as $item)
-            @if($item instanceof \Illuminate\Contracts\Support\Renderable)
-                {!! $item->render() !!}
-            @else
-                {!! $item !!}
-            @endif
-        @endforeach
-    </div>
+    @foreach ($items as $item)
+        @if($item instanceof \Illuminate\Contracts\Support\Renderable)
+            {!! $item->render() !!}
+        @else
+            {!! $item !!}
+        @endif
+    @endforeach
 @endif
