868d68ad1d2b751c7cfb85467fc3504d674b2042
Atmden <atmden@gmail.com>
2016-12-19 22:25:16 +0500

    Добавил setWidth, getWidth и width в toArray()

--- src/Form/Element/NamedFormElement.php
+++ src/Form/Element/NamedFormElement.php
@@ -489,6 +489,7 @@ abstract class NamedFormElement extends FormElement
             'readonly' => $this->isReadonly(),
             'value' => $this->getValue(),
             'helpText' => $this->getHelpText(),
+            'width' => $this->getWidth(),
             'required' => in_array('required', $this->validationRules),
         ];
     }
