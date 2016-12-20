00f431d6897db07fe10547609d1ea8846dc9cb70
Butschster <butschster@gmail.com>
2016-12-20 12:53:14 +0300

    fix navigation

--- resources/views/default/_partials/navigation/page.blade.php
+++ resources/views/default/_partials/navigation/page.blade.php
@@ -1,27 +1,42 @@
 @if($hasChild)
-<li {!! $attributes !!}>
-    <a href="#" >
-        {!! $icon !!}
-        <span>{!! $title !!}</span>
-        <span class="pull-right-container">
+    <li {!! $attributes !!}>
+        <a href="#" >
+            {!! $icon !!}
+            <span>{!! $title !!}</span>
+            <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
-        </span>
-    </a>
 
-    <ul class="treeview-menu">
-        @foreach($pages as $page)
-           {!! $page->render() !!}
-        @endforeach
-    </ul>
-</li>
-@else
-<li {!! $attributes !!}>
-    <a href="{{ $url }}">
-        {!! $icon !!}
-        <span>{!! $title !!}</span>
-        <span class="pull-right-container">
-              {!! $badge !!}
+                @if($badges->count() > 0)
+                    <span class="sidebar-page-badges">
+                        @foreach($badges as $badge)
+                            {!! $badge->render() !!}
+                        @endforeach
+                    </span>
+                @endif
         </span>
-    </a>
-</li>
+        </a>
+
+        <ul class="treeview-menu">
+            @foreach($pages as $page)
+                {!! $page->render() !!}
+            @endforeach
+        </ul>
+    </li>
+@else
+    <li {!! $attributes !!}>
+        <a href="{{ $url }}">
+            {!! $icon !!}
+            <span>{!! $title !!}</span>
+
+            @if($badges->count() > 0)
+                <span class="pull-right">
+                    <span class="sidebar-page-badges">
+                    @foreach($badges as $badge)
+                        {!! $badge->render() !!}
+                    @endforeach
+                    </span>
+                </span>
+            @endif
+        </a>
+    </li>
 @endif
