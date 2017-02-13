7c238deee0152dc34b1a65e469c1936a1a631a43
Aios <aioslike@gmail.com>
2017-02-13 18:21:24 +0200

    Push ajax selects

--- src/Factories/FormElementFactory.php
+++ src/Factories/FormElementFactory.php
@@ -35,6 +35,8 @@ use SleepingOwl\Admin\Contracts\Form\FormElementFactoryInterface;
  * @method Element\Upload upload($name, $label = null)
  * @method Element\Number number($name, $label = null)
  * @method Element\DependentSelect dependentselect($name, $label = null, array|Model $options)
+ * @method Element\MultiSelectAjax multiselectajax($name, $label = null, array|Model $options)
+ * @method Element\SelectAjax selectajax($name, $label = null, array|Model $options)
  */
 class FormElementFactory extends AliasBinder implements FormElementFactoryInterface
 {
@@ -75,6 +77,8 @@ class FormElementFactory extends AliasBinder implements FormElementFactoryInterf
             'html' => Element\Html::class,
             'number' => Element\Number::class,
             'dependentselect' => Element\DependentSelect::class,
+            'selectajax' => Element\SelectAjax::class,
+            'multiselectajax' => Element\MultiSelectAjax::class,
         ]);
     }
 }
