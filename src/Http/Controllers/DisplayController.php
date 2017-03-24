09eabbde20381f63158e47e99f78bfc8c36e70b4
Aios <aioslike@gmail.com>
2017-03-24 03:36:04 +0200

    Fix DatatableAsync in Tabs
    fixed #419
    fixed #483

--- src/Http/Controllers/DisplayController.php
+++ src/Http/Controllers/DisplayController.php
@@ -10,41 +10,66 @@ use SleepingOwl\Admin\Display\DisplayTabbed;
 use Illuminate\Contracts\Foundation\Application;
 use SleepingOwl\Admin\Display\DisplayDatatablesAsync;
 use SleepingOwl\Admin\Contracts\ModelConfigurationInterface;
+use SleepingOwl\Admin\Form\Columns\Column;
+use SleepingOwl\Admin\Form\FormElements;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DisplayController extends Controller
 {
+
     /**
      * @param ModelConfigurationInterface $model
      * @param Request $request
      * @param Application $application
      * @param null $name
+     * @throws NotFoundHttpException
      *
      * @return JsonResponse
      */
     public function async(ModelConfigurationInterface $model, Request $request, Application $application, $name = null)
     {
         $display = $model->fireDisplay();
-
         if ($display instanceof DisplayTabbed) {
             $tabs = $display->getTabs();
+
             foreach ($tabs as $tab) {
                 $content = $tab->getContent();
-                if ($content instanceof DisplayDatatablesAsync && $content->getName() === $name) {
-                    return $content;
+
+                if ($content instanceof FormElements) {
+                    foreach ($content->getElements() as $element) {
+
+                        //Return data-table if inside FormElements
+                        if ($element instanceof DisplayDatatablesAsync) {
+                            if ($element->getName() == $name) {
+                                return $this->renderFindedTable($element, $application, $request);
+                            }
+                        }
+
+                        //Try to find data table in columns
+                        if ($element instanceof Column) {
+                            foreach($element->getElements() as $columnElement){
+                                if ($columnElement instanceof DisplayDatatablesAsync) {
+                                    if($columnElement->getName() == $name){
+                                        return $this->renderFindedTable($columnElement, $application, $request);
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
+
+                //Finded trully in content-tab
+                if ($content instanceof DisplayDatatablesAsync) {
+                    if ($content->getName() == $name) {
+                        return $this->renderFindedTable($content, $application, $request);
+                    }
+                }
+
             }
         }
 
         if ($display instanceof DisplayDatatablesAsync) {
-            try {
-                return $display->renderAsync($request);
-            } catch (\Exception $exception) {
-                return new JsonResponse([
-                    'message'  => $application->isLocal()
-                        ? $exception->getMessage()
-                        : trans('sleeping_owl::lang.table.error'),
-                ], 403);
-            }
+            return $this->renderFindedTable($display, $application, $request);
         }
 
         abort(404);
@@ -71,6 +96,25 @@ class DisplayController extends Controller
             $display->getRepository()->reorder(
                 $request->input('data')
             );
+        }
+    }
+
+    /**
+     * @param DisplayDatatablesAsync $datatable
+     * @param Application $application
+     * @param Request $request
+     * @return array|JsonResponse
+     */
+    protected function renderFindedTable(DisplayDatatablesAsync $datatable, Application $application, Request $request)
+    {
+        try {
+            return $datatable->renderAsync($request);
+        } catch (\Exception $exception) {
+            return new JsonResponse([
+                'message' => $application->isLocal()
+                    ? $exception->getMessage()
+                    : trans('sleeping_owl::lang.table.error'),
+            ], 403);
         }
     }
 }
