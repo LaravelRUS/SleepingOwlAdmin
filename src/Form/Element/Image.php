51d2503bce5879b193cc81e6a83ad658fc2281a7
Dave <aioslike@gmail.com>
2017-04-04 16:47:50 +0000

    Apply fixes from StyleCI

--- src/Form/Element/Image.php
+++ src/Form/Element/Image.php
@@ -7,7 +7,6 @@ use Illuminate\Validation\Validator;
 
 class Image extends File
 {
-
     /**
      * @var string
      */
@@ -39,7 +38,7 @@ class Image extends File
 
             $size = getimagesize($file->getRealPath());
 
-            if (!$size) {
+            if (! $size) {
                 $validator->errors()->add('file', trans('sleeping_owl::validation.not_image'));
             }
         });
@@ -79,14 +78,14 @@ class Image extends File
             return $callback($file, $path, $filename, $settings);
         }
 
-        if (class_exists('Intervention\Image\Facades\Image') and (bool)getimagesize($file->getRealPath())) {
+        if (class_exists('Intervention\Image\Facades\Image') and (bool) getimagesize($file->getRealPath())) {
             $image = \Intervention\Image\Facades\Image::make($file);
 
             foreach ($settings as $method => $args) {
                 call_user_func_array([$image, $method], $args);
             }
 
-            $value = $path . '/' . $filename;
+            $value = $path.'/'.$filename;
 
             $image->save($value);
 
