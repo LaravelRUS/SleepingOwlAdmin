8fd6f787d1ef8b8defcd884931d83f0349ebf355
mavsan <mavsan@yandex.ru>
2017-04-08 18:38:41 +1000

    Update ModelRouter.php
    Лишние пробелы, т.к. не проходит проверку стиля.

--- src/Routing/ModelRouter.php
+++ src/Routing/ModelRouter.php
@@ -65,7 +65,7 @@ class ModelRouter
 
             if ($model->hasCustomControllerClass() && $route->getActionName() !== 'Closure') {
                 list($controller, $action) = explode('@', $route->getActionName(), 2);
-                
+
                 if(method_exists($model->getControllerClass(), $action)) {
                     $this->runCustomController($route, $model->getControllerClass(), $action);
                 }
