c83376fb37f70838e73a7f56f80e46090f67041f
Aios <aioslike@gmail.com>
2017-06-21 03:58:27 +0300

    Fixed #614

--- resources/assets/js/admin/form/file.js
+++ resources/assets/js/admin/form/file.js
@@ -19,7 +19,7 @@ Vue.component('element-file', Vue.extend({
         return {
             errors: [],
             uploading: false,
-            val: false,
+            val: '',
         }
     },
     mounted () {
@@ -60,7 +60,7 @@ Vue.component('element-file', Vue.extend({
             var self = this;
 
             Admin.Messages.confirm(trans('lang.message.are_you_sure')).then(() => {
-                self.val = false;
+                self.val = '';
             });
         },
         closeAlert () {
