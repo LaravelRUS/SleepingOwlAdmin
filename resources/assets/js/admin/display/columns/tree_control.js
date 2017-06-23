9e1075d67972fb1e81b8b0914d8de02e2f692be5
Aios <aioslike@gmail.com>
2017-06-23 21:57:23 +0300

    Added to confirm event selector argument

--- resources/assets/js/admin/display/columns/tree_control.js
+++ resources/assets/js/admin/display/columns/tree_control.js
@@ -6,11 +6,11 @@ Admin.Modules.register('display.columns.tree_control', () => {
             let $form = $(this).closest('form');
 
             Admin.Messages.confirm(question).then(() => {
-                Admin.Events.fire("datatables::confirm::submitting", $form);
+                Admin.Events.fire("datatables::confirm::submitting", $form, selector);
                 $form.submit()
-                Admin.Events.fire("datatables::confirm::submitted", $form);
+                Admin.Events.fire("datatables::confirm::submitted", $form, selector);
             }, dismiss => {
-                Admin.Events.fire("datatables::confirm::cancel", $form);
+                Admin.Events.fire("datatables::confirm::cancel", $form, selector);
             });
         });
     };
