3671ce7bfec4e132f47ee308cf472f11289bf496
Alexej <lunatik31@yandex.ru>
2017-06-30 17:33:58 +0300

    Create Select2 Editable Column
    Create Select2 Editable Column

---
+++ resources/views/default/column/editable/select2.blade.php
@@ -0,0 +1,13 @@
+<a href="#"
+    class="inline-editable"
+    data-name="{{ $name }}"
+    data-value="{{ $value }}"
+    data-url="{{ $url }}"
+    data-type="select2"
+    data-pk="{{ $id }}"
+    data-title="{{ $headerTitle }}"
+    data-data= "{id: 1, name: 'text1'}"
+    style="text-align:right; color: green;"
+>{{ $value }}</a>
+
+{!! $append !!}
