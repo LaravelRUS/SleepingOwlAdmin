cf02f2a0681a2a88a15ceb1a4760cd9a69a547f3
Alexej <lunatik31@yandex.ru>
2017-06-30 16:56:05 +0300

    Create textarea editable blade
    Create textarea editable blade

---
+++ resources/views/default/column/editable/textarea.blade.php
@@ -0,0 +1,13 @@
+<a href="#"
+    class="inline-editable"
+    data-name="{{ $name }}"
+    data-value="{{ $value }}"
+    data-url="{{ $url }}"
+    data-type="textarea"
+    data-pk="{{ $id }}"
+
+    {{ $attributes }}
+
+>{{ $value }}</a>
+
+{!! $append !!}
