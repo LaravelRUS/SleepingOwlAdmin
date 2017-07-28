68d457e07d4231ac96f7f891ca04607457e36605
Aios <aioslike@gmail.com>
2017-07-28 06:21:15 +0300

    Added indicator of deleted model

--- resources/views/default/column/control.blade.php
+++ resources/views/default/column/control.blade.php
@@ -1,3 +1,6 @@
 @foreach($buttons as $button)
-	{!! $button->render() !!}
+    {!! $button->render() !!}
 @endforeach
+@if($model && $model->deleted_at)
+    <span class="model_deleted" style="display:none"></span>
+@endif
