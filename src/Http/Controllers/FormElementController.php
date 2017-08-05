ed7fe5f96d121bf457977ce188e514240dc71e49
Aios <aioslike@gmail.com>
2017-08-05 11:51:21 +0300

    Fix duplicate

--- src/Http/Controllers/FormElementController.php
+++ src/Http/Controllers/FormElementController.php
@@ -11,6 +11,34 @@ use SleepingOwl\Admin\Contracts\ModelConfigurationInterface;
 
 class FormElementController extends Controller
 {
+
+    /**
+     * @param ModelConfigurationInterface $model
+     * @param null $id
+     * @return JsonResponse|mixed
+     */
+    public function getModelLogic(ModelConfigurationInterface $model, $id = null)
+    {
+        if (! is_null($id)) {
+            $item = $model->getRepository()->find($id);
+            if (is_null($item) || ! $model->isEditable($item)) {
+                return new JsonResponse([
+                    'message' => trans('lang.message.access_denied'),
+                ], 403);
+            }
+
+            return $model->fireEdit($id);
+        }
+
+        if (! $model->isCreatable()) {
+            return new JsonResponse([
+                'message' => trans('lang.message.access_denied'),
+            ], 403);
+        }
+
+        return $model->fireCreate();
+    }
+
     /**
      * @param Request $request
      * @param ModelConfigurationInterface $model
@@ -21,23 +49,10 @@ class FormElementController extends Controller
      */
     public function dependentSelect(Request $request, ModelConfigurationInterface $model, $field, $id = null)
     {
-        if (! is_null($id)) {
-            $item = $model->getRepository()->find($id);
-            if (is_null($item) || ! $model->isEditable($item)) {
-                return new JsonResponse([
-                    'message' => trans('lang.message.access_denied'),
-                ], 403);
-            }
+        $form = $this->getModelLogic($model, $id);
 
-            $form = $model->fireEdit($id);
-        } else {
-            if (! $model->isCreatable()) {
-                return new JsonResponse([
-                    'message' => trans('lang.message.access_denied'),
-                ], 403);
-            }
-
-            $form = $model->fireCreate();
+        if ($form instanceof JsonResponse) {
+            return $form;
         }
 
         /** @var DependentSelect $element */
@@ -60,7 +75,7 @@ class FormElementController extends Controller
         }
 
         return new JsonResponse([
-            'output' => collect($options)->map(function ($value, $key) {
+            'output'   => collect($options)->map(function ($value, $key) {
                 return ['id' => $key, 'name' => $value];
             }),
             'selected' => $element->getValueFromModel(),
@@ -77,23 +92,10 @@ class FormElementController extends Controller
      */
     public function multiselectSearch(Request $request, ModelConfigurationInterface $model, $field, $id = null)
     {
-        if (! is_null($id)) {
-            $item = $model->getRepository()->find($id);
-            if (is_null($item) || ! $model->isEditable($item)) {
-                return new JsonResponse([
-                    'message' => trans('lang.message.access_denied'),
-                ], 403);
-            }
+        $form = $this->getModelLogic($model, $id);
 
-            $form = $model->fireEdit($id);
-        } else {
-            if (! $model->isCreatable()) {
-                return new JsonResponse([
-                    'message' => trans('lang.message.access_denied'),
-                ], 403);
-            }
-
-            $form = $model->fireCreate();
+        if ($form instanceof JsonResponse) {
+            return $form;
         }
 
         /** @var DependentSelect $element */
@@ -133,23 +135,10 @@ class FormElementController extends Controller
      */
     public function selectSearch(Request $request, ModelConfigurationInterface $model, $field, $id = null)
     {
-        if (! is_null($id)) {
-            $item = $model->getRepository()->find($id);
-            if (is_null($item) || ! $model->isEditable($item)) {
-                return new JsonResponse([
-                    'message' => trans('lang.message.access_denied'),
-                ], 403);
-            }
+        $form = $this->getModelLogic($model, $id);
 
-            $form = $model->fireEdit($id);
-        } else {
-            if (! $model->isCreatable()) {
-                return new JsonResponse([
-                    'message' => trans('lang.message.access_denied'),
-                ], 403);
-            }
-
-            $form = $model->fireCreate();
+        if ($form instanceof JsonResponse) {
+            return $form;
         }
 
         /** @var DependentSelect $element */
