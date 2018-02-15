8270f0767d5d8d34e66f35f60233bba9860d830e
Aglok <perlovav@gmail.com>
2018-02-15 03:19:33 +0300

    update navigation.php

--- src/Navigation.php
+++ src/Navigation.php
@@ -108,4 +108,32 @@ class Navigation extends \KodiComponents\Navigation\Navigation implements Naviga
 
         return false;
     }
+    
+    /**
+     * Переопределяем метод toArray(), вносим класс, treeview для активации меню tree в adminlte 2.4
+     * @return array
+     */
+    public function toArray()
+    {
+        if ($this->isActive() and ! $this->hasClassProperty($class = config('navigation.class.active', 'active'))) {
+            $this->setHtmlAttribute('class', $class);
+        }
+        if ($this->hasChild() and ! $this->hasClassProperty($class = config('navigation.class.has_child', 'treeview'))) {
+            $this->setHtmlAttribute('class', $class);
+        }
+        return parent::toArray() + [
+                'hasChild' => $this->hasChild(),
+                'id' => $this->getId(),
+                'title' => $this->getTitle(),
+                'icon' => $this->getIcon(),
+                'priority' => $this->getPriority(),
+                'url' => $this->getUrl(),
+                'path' => $this->getPath(),
+                'isActive' => $this->isActive(),
+                'attributes' => $this->htmlAttributesToString(),
+                'badges' => $this->getBadges()->sortBy(function (BadgeInterface $badge) {
+                    return $badge->getPriority();
+                }),
+            ];
+    }
 }
