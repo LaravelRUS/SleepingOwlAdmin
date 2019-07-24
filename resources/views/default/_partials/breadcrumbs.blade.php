329f5f6b966d9b29b1f0fdaf3d9a360fdefae4b7
Daan <daan@ukr.net>
2019-07-24 15:34:38 +0300

    Fix merged file

--- resources/views/default/_partials/breadcrumbs.blade.php
+++ resources/views/default/_partials/breadcrumbs.blade.php
@@ -1,13 +1,8 @@
 @if ($breadcrumbs)
     <ol class="breadcrumb float-sm-right">
         @foreach ($breadcrumbs as $breadcrumb)
-<<<<<<< HEAD
             @if ($breadcrumb->url && !$breadcrumb->last)
                 <li class="breadcrumb-item"><a href="{{{ $breadcrumb->url }}}">{{{ $breadcrumb->title }}}</a></li>
-=======
-            @if ($breadcrumb->url && !$loop->last)
-                <li><a href="{{{ $breadcrumb->url }}}">{{{ $breadcrumb->title }}}</a></li>
->>>>>>> development
             @else
                 <li class="breadcrumb-item active">{{{ $breadcrumb->title }}}</li>
             @endif
