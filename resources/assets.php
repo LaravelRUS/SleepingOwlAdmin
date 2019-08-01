bb4cea97ea7745f5e3764178188004971e11e83c
Daan <daan@ukr.net>
2019-08-02 01:58:47 +0300

    Fix assets for BS4

--- resources/assets.php
+++ resources/assets.php
@@ -22,6 +22,6 @@ $this->app['sleeping_owl.wysiwyg']->register('summernote', new \SleepingOwl\Admi
   ->js('mdwn3', '//cdnjs.cloudflare.com/ajax/libs/codemirror/3.20.0/codemirror.js', null, true)
   ->js('mdwn4', '//cdnjs.cloudflare.com/ajax/libs/codemirror/3.20.0/mode/xml/xml.js', null, true)
   ->js('mdwn5', '//cdnjs.cloudflare.com/ajax/libs/codemirror/2.36.0/formatting.js', null, true)
-  ->js(null, '//cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.min.js', null, true)
+  ->js(null, '//cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.min.js', null, true)
   ->js('lang', '//cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/lang/summernote-ru-RU.min.js', null, true)
-  ->css(null, '//cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css');
+  ->css(null, '//cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.css');
