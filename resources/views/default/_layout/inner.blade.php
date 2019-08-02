e28f6e745594909897332a8c6a9ae75844e2739a
Davidnadejdin <Davidnadejdin@gmail.com>
2019-08-02 15:07:27 +0500

    Подправил сайдабар

--- resources/views/default/_layout/inner.blade.php
+++ resources/views/default/_layout/inner.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 	<div class="wrapper" id="vueApp">
-		<nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+		<nav class="main-header navbar navbar-expand bg-white navbar-light">
 			@include(AdminTemplate::getViewPath('_partials.header'))
 		</nav>
 
--- resources/views/default/_partials/navigation/navigation.blade.php
+++ resources/views/default/_partials/navigation/navigation.blade.php
@@ -1,3 +1,7 @@
-@foreach($pages as $page)
-    {!! $page->render() !!}
-@endforeach
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        @foreach($pages as $page)
+            {!! $page->render() !!}
+        @endforeach
+    </ul>
+</nav>
