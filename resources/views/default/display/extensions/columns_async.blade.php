c16361823c5426b58805998a34c16b387d71ceae
Davidnadejdin <Davidnadejdin@gmail.com>
2019-08-02 15:21:03 +0500

    Обренул asyncDatables в карточку

--- resources/views/default/display/extensions/columns_async.blade.php
+++ resources/views/default/display/extensions/columns_async.blade.php
@@ -1,20 +1,26 @@
-<table {!! $attributes !!}>
-    <colgroup>
-        @foreach ($columns as $column)
-            @continue(!$column->isVisible())
-            <col width="{!! $column->getWidth() !!}" />
-        @endforeach
-    </colgroup>
-    <thead>
-    <tr>
-        @foreach ($columns as $column)
-            <th {!! $column->getHeader()->htmlAttributesToString() !!}>
-                {!! $column->getHeader()->render() !!}
-            </th>
-        @endforeach
-    </tr>
-    </thead>
-    @yield('table.header')
-    @yield('table.footer')
-</table>
+<div class="card">
+    <!-- /.card-header -->
+    <div class="card-body">
+        <table {!! $attributes !!}>
+            <colgroup>
+                @foreach ($columns as $column)
+                    @continue(!$column->isVisible())
+                    <col width="{!! $column->getWidth() !!}" />
+                @endforeach
+            </colgroup>
+            <thead>
+            <tr>
+                @foreach ($columns as $column)
+                    <th {!! $column->getHeader()->htmlAttributesToString() !!}>
+                        {!! $column->getHeader()->render() !!}
+                    </th>
+                @endforeach
+            </tr>
+            </thead>
+            @yield('table.header')
+            @yield('table.footer')
+        </table>
+    </div>
+    <!-- /.card-body -->
+</div>
 
