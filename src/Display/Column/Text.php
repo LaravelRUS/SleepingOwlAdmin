4836b618cf1310ae5e8c94e95c70bed2b7915e8f
reserved <reserved@mail.ru>
2019-08-07 18:46:13 +0300

    Allow to modify Text column output value, via setModifier method

--- src/Display/Column/Text.php
+++ src/Display/Column/Text.php
@@ -10,17 +10,49 @@ class Text extends NamedColumn
     protected $view = 'column.text';
 
     /**
+     * @var mixed
+     */
+    protected $modifier = null;
+
+    /**
      * @var bool
      */
     protected $isSearchable = true;
+
+
+    /**
+     * @return \Closure|mixed
+     */
+    public function getModifier()
+    {
+        return $this->modifier;
+    }
+
+    /**
+     * @param \Closure|mixed $modifier
+     *
+     * @return $this
+     */
+    public function setModifier($modifier)
+    {
+        $this->modifier = $modifier;
+
+        return $this;
+    }
 
     /**
      * @return array
      */
     public function toArray()
     {
+        $model_value = $this->getModelValue();
+        if (is_callable($modifier = $this->getModifier()))
+        {
+            $model_value = $modifier($model_value);
+        }
+
         return parent::toArray() + [
-                'value' => $this->getModelValue(),
+                'value' => $model_value,
                 'small' => $this->getModelSmallValue(),
             ];
     }
