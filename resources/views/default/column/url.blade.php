3de8b19afd8ce5e3ef32bd472029ea9df53aa340
Daan <daan@ukr.net>
2019-08-13 19:16:43 +0300

    Rebuild assets

--- resources/views/default/column/url.blade.php
+++ resources/views/default/column/url.blade.php
@@ -1,6 +1,8 @@
-@if (!empty($value))
-<a href="{{ $value }}" {{ app('html')->attributes($linkAttributes) }}>
-	<i class="fas fa-link" data-toggle="tooltip" title="{{ trans('sleeping_owl::lang.table.filter-goto') }}"></i>
-</a>
-@endif
-{!! $append !!}
+<div class="text-center">
+	@if (!empty($value))
+		<a href="{{ $value }}" {{ app('html')->attributes($linkAttributes) }}>
+			<i class="fas fa-link" data-toggle="tooltip" title="{{ trans('sleeping_owl::lang.table.filter-goto') }}"></i>
+		</a>
+	@endif
+	{!! $append !!}
+</div>
