cbcc63014dc58a589d1ae3195055474758fc9dcb
Daan <daan@ukr.net>
2019-09-18 22:46:54 +0300

    Fix loop

--- resources/views/default/_partials/breadcrumbs.blade.php
+++ resources/views/default/_partials/breadcrumbs.blade.php
@@ -1,7 +1,7 @@
 @if ($breadcrumbs)
     <ol class="breadcrumb float-sm-right">
         @foreach ($breadcrumbs as $breadcrumb)
-            @if ($breadcrumb->url && !$breadcrumb->last)
+            @if ($breadcrumb->url && !$loop->last)
                 <li class="breadcrumb-item"><a href="{{{ $breadcrumb->url }}}">{!! $breadcrumb->title !!}</a></li>
             @else
                 <li class="breadcrumb-item active">{!! $breadcrumb->title !!}</li>
