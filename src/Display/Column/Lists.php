5b9a3897416d78cfd1f11224b9b3413a41a1b416
i p <spamarmy@yandex.ru>
2019-11-03 12:26:58 +0500

    [add] setSortable to Column/Lists

--- src/Display/Column/Lists.php
+++ src/Display/Column/Lists.php
@@ -2,6 +2,8 @@
 
 namespace SleepingOwl\Admin\Display\Column;
 
+use Illuminate\Support\Collection;
+
 class Lists extends NamedColumn
 {
     /**
@@ -18,6 +20,49 @@ class Lists extends NamedColumn
      * @var bool
      */
     protected $isSearchable = false;
+
+    /**
+     * @var bool
+     */
+    protected $sortable = false;
+
+    /**
+     * @param bool $sortable
+     *
+     * @return $this
+     */
+    public function setSortable($sortable)
+    {
+        $this->sortable = (bool)$sortable;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSortable()
+    {
+        return $this->sortable;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModelValue()
+    {
+        $values = parent::getModelValue();
+
+        if ($this->isSortable()) {
+            if ($values instanceof Collection) {
+                $values = $values->sort();
+            } elseif (is_array($values)) {
+                asort($values);
+            }
+        }
+
+        return $values;
+    }
 
     /**
      * @return array
