b9483e414ee97b4767f3fd6b3f0ef202dd63f2e4
Alexej <lunatik31@yandex.ru>
2019-11-23 01:05:24 +0300

    update display tree to neasted2

--- resources/views/default/display/tree.blade.php
+++ resources/views/default/display/tree.blade.php
@@ -1,6 +1,38 @@
 @yield('before.card')
 @yield('before.panel')
 
+<div class="dd nestable" id="nestable3">
+    <ol class="dd-list">
+        <li class="dd-item dd3-item" data-id="13">
+            <div class="dd-handle dd3-handle">Drag</div>
+            <div class="dd3-content">Item 13</div>
+        </li>
+        <li class="dd-item dd3-item" data-id="14">
+            <div class="dd-handle dd3-handle">Drag</div>
+            <div class="dd3-content">Item 14</div>
+        </li>
+        <li class="dd-item dd3-item" data-id="15">
+            <div class="dd-handle dd3-handle">Drag</div>
+            <div class="dd3-content">Item 15</div>
+            <ol class="dd-list">
+                <li class="dd-item dd3-item" data-id="16">
+                    <div class="dd-handle dd3-handle">Drag</div>
+                    <div class="dd3-content">Item 16</div>
+                </li>
+                <li class="dd-item dd3-item" data-id="17">
+                    <div class="dd-handle dd3-handle">Drag</div>
+                    <div class="dd3-content">Item 17</div>
+                </li>
+                <li class="dd-item dd3-item" data-id="18">
+                    <div class="dd-handle dd3-handle">Drag</div>
+                    <div class="dd3-content">Item 18</div>
+                </li>
+            </ol>
+        </li>
+    </ol>
+</div>
+
+
 <div class="card card-default {!! $card_class !!}">
     <div class="card-heading card-header">
         @if ($creatable)
