98ae642a15be50bd33796630eacd8e7454e6e59c
karen <isr.kar@gmail.com>
2020-07-09 16:29:46 +0300

    исправляет задвоение окошка выбора даты issue #1207

--- resources/views/default/column/filter/date.blade.php
+++ resources/views/default/column/filter/date.blade.php
@@ -1,19 +1,16 @@
-<div class="input-group input-date form-group date">
-		<div class="input-group input-date date form-group">
-			<input
-			data-date-format="{{ $pickerFormat }}"
-			data-date-useseconds="{{ $seconds ? 'true' : 'false' }}"
-			class="form-control column-filter"
-			type="text"
-			placeholder="{{ $placeholder }}"
-			aria-label="{{ $placeholder }}"
-			{!! $attributes !!} />
+<div class="input-group input-date date form-group">
+    <input
+        data-date-format="{{ $pickerFormat }}"
+        data-date-useseconds="{{ $seconds ? 'true' : 'false' }}"
+        class="form-control column-filter"
+        type="text"
+        placeholder="{{ $placeholder }}"
+        aria-label="{{ $placeholder }}"
+        {!! $attributes !!} />
 
-			<div class="input-group-prepend input-group-addon">
-				<div class="input-group-text">
-					<span class="far fa-calendar-alt"></span>
-				</div>
-			</div>
-		</div>
-
+    <div class="input-group-prepend input-group-addon">
+        <div class="input-group-text">
+            <span class="far fa-calendar-alt"></span>
+        </div>
+    </div>
 </div>
