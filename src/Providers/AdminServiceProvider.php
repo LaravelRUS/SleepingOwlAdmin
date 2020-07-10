70e20fc82687f9aaf476c5ad25db83991e4868a9
Alexey <butochnikov@gmail.com>
2020-07-11 02:16:35 +0300

    Fix `Target class [assets] does not exist.` error

--- src/Providers/AdminServiceProvider.php
+++ src/Providers/AdminServiceProvider.php
@@ -78,12 +78,14 @@ class AdminServiceProvider extends ServiceProvider
         $this->app->singleton('assets.packages', function ($app) {
             return new \KodiCMS\Assets\PackageManager();
         });
+        
+        $this->app->singleton('assets', function ($app) {
+            return new Assets($app['assets.packages']);
+        });
 
         $this->app->singleton('sleeping_owl.meta', function ($app) {
             return new Meta(
-                new Assets(
-                    $app['assets.packages']
-                )
+                $app['assets']
             );
         });
 
