b8a9d90e4d991c91a1a6ebbc9d92bf23d3262ae7
Alexey <lunatik31@yandex.ru>
2020-12-01 14:48:47 +0300

    Index Column
    Create counter column that contains the position for each row in the table

---
+++ src/Display/Column/Index.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SleepingOwl\Admin\Display\Column;
+
+use Closure;
+
+class Index extends NamedColumn
+{
+    /**
+     * @var string
+     */
+    protected $view = 'column.custom';
+
+    /**
+     * @var string
+     */
+    protected $width = '45px';
+
+    /**
+     * @var bool
+     */
+    protected $orderable = false;
+
+    /**
+     * @var bool
+     */
+    protected $isSearchable = false;
+
+    /**
+     * Custom constructor.
+     *
+     * @param null|string $label
+     * @param Closure $callback
+     */
+    public function __construct($label = null)
+    {
+        parent::__construct($label);
+        if (! is_null($label)) {
+            $this->setLabel($label);
+        }
+    }
+
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    public function toArray()
+    {
+        return parent::toArray() + [
+            'value' => ++request()->start,
+        ];
+    }
+}
