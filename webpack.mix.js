097facaf60bf92cf99c87a04346f4fd9606618f9
reserved <reserved@mail.ru>
2021-03-23 15:42:54 +0300

    Rebuild assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,17 @@
 let mix = require('laravel-mix');
 
-mix.setPublicPath('./public/packages/sleepingowl/default/');
-mix
-  .less('resources/assets/less/common.less', 'css/admin-app.css')
+// mix.setPublicPath('./public/packages/sleepingowl/default/');
+mix.setPublicPath('./public/default/');
 
+mix.less('resources/assets/less/common.less', 'css/admin-app.css')
 mix.js('resources/assets/js_owl/app.js', 'js/admin-app.js');
 mix.js('resources/assets/js_owl/vue_init.js', 'js/vue.js');
 mix.js('resources/assets/js_owl/modules_load.js', 'js/modules.js')
-.options({
-    processCssUrls: true,
-    resourceRoot: '../',
-    imgLoaderOptions: {
-      enabled: false,
-    }
-});
+    .options({
+        processCssUrls: true,
+        resourceRoot: '../',
+        imgLoaderOptions: {
+            enabled: false,
+        }
+    });
 
