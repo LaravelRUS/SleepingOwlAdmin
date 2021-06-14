0df20735c536dcdf7f3d2968c7158bd06ddf7294
reserved <reserved@mail.ru>
2021-06-14 20:40:34 +0300

    Use mix version() method, apply versioning on TemplateDefault.php, rebuild assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,19 +3,20 @@ let mix = require('laravel-mix');
 mix.setPublicPath('./public/default/');
 
 mix
-    .sass('resources/assets/scss/app.scss',        'css/admin-app.css')
-    .js('resources/assets/js_owl/app.js',          'js/admin-app.js')
-    .js('resources/assets/js_owl/vue_init.js',     'js/vue.js')
+    .sass('resources/assets/scss/app.scss', 'css/admin-app.css')
+    .js('resources/assets/js_owl/app.js', 'js/admin-app.js')
+    .js('resources/assets/js_owl/vue_init.js', 'js/vue.js')
     .js('resources/assets/js_owl/modules_load.js', 'js/modules.js')
 
-
-    .copy('public/default', '../../../public/packages/sleepingowl/default')
-
+    /* copy() and version() was conflicted */
+    // .copy('public/default', '../../../public/packages/sleepingowl/default')
+    .version()
 
     .options({
-      processCssUrls: true,
-      resourceRoot: '../',
-      imgLoaderOptions: {
-        enabled: false,
-      },
-    });
+        processCssUrls: true,
+        resourceRoot: '../',
+        imgLoaderOptions: {
+            enabled: false,
+        },
+    })
+;
