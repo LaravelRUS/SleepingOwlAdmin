3e10c876d719a53030e91a6f3a7c98422073b742
reserved <reserved@mail.ru>
2021-06-14 21:51:13 +0300

    Fix for Datatables thead/tfoot

--- resources/views/default/display/extensions/columns.blade.php
+++ resources/views/default/display/extensions/columns.blade.php
@@ -1,46 +1,46 @@
 <div class="panel-table card-body pt-0 pl-0 pr-0">
 
-  <table {!! $attributes !!}>
-      <colgroup>
-          @foreach ($columns as $column)
-              <col width="{!! $column->getWidth() !!}"/>
-          @endforeach
-      </colgroup>
+    <table {!! $attributes !!}>
+        <colgroup>
+            @foreach ($columns as $column)
+                <col width="{!! $column->getWidth() !!}"/>
+            @endforeach
+        </colgroup>
 
-      <thead>
-      <tr>
-          @foreach ($columns as $column)
-              <th {!! $column->getHeader()->htmlAttributesToString() !!}>
-                  {!! $column->getHeader()->render() !!}
-              </th>
-          @endforeach
-      </tr>
-      </thead>
+        <thead>
+        <tr>
+            @foreach ($columns as $column)
+                <th {!! $column->getHeader()->htmlAttributesToString() !!}>
+                    {!! $column->getHeader()->render() !!}
+                </th>
+            @endforeach
+        </tr>
+        </thead>
 
-      @yield('table.header')
-      <tbody>
-      @foreach ($collection as $model)
-          <tr>
-              @foreach ($columns as $column)
-                  @php
-                      $column->setModel($model);
+        @yield('table.header')
+        <tbody>
+        @foreach ($collection as $model)
+            <tr>
+                @foreach ($columns as $column)
+                    @php
+                        $column->setModel($model);
 
-                      if ($column instanceof \SleepingOwl\Admin\Display\Column\Control) {
-                          $column->initialize();
-                      }
-                  @endphp
+                        if ($column instanceof \SleepingOwl\Admin\Display\Column\Control) {
+                            $column->initialize();
+                        }
+                    @endphp
 
-                  <td v-pre>
-                      {!! $column->render() !!}
-                  </td>
+                    <td v-pre>
+                        {!! $column->render() !!}
+                    </td>
 
-              @endforeach
-          </tr>
-      @endforeach
-      </tbody>
+                @endforeach
+            </tr>
+        @endforeach
+        </tbody>
 
-      @yield('table.footer')
-  </table>
+        @yield('table.footer')
+    </table>
 
 </div>
 @if(!is_null($pagination))
