ba6aabeb18a8aacc410e303f55da34e0003d0087
reserved <reserved@mail.ru>
2021-07-19 16:16:13 +0300

    actions.js - use server answer in __callback; __redirect feature

--- resources/assets/js_owl/admin/display/actions.js
+++ resources/assets/js_owl/admin/display/actions.js
@@ -1,41 +1,36 @@
 Admin.Modules.register('display.actions', () => {
     $('form[data-type="display-actions"]').on('submit', function (e) {
-
-      let action_value = $("#sleepingOwlActionsStore").find('option:selected').val();
-
-      if (action_value == 0) {
-        Swal.fire({
-          title: trans('lang.table.no-action'),
-          text: trans('lang.select.nothing'),
-          icon: 'error',
-          timer: 5000
-        })
-        return false;
-      }
-
         e.preventDefault();
         let self = $(this);
 
-
-        let $checkboxes = $('.adminCheckboxRow').filter(':checked'),
-        $selectActions = $("#sleepingOwlActionsStore");
-
-
-        if ($checkboxes.length == 0) {
-          Swal.fire({
-            title: trans('lang.select.nothing'),
-            text: trans('lang.select.no_items'),
-            icon: 'error',
-            timer: 5000
-          })
-
-          return false;
+        /**
+         * @todo Need to refactor selectors for use properly instance of the form, because it can be used inside one of several tabs
+         */
+        let action_value = $("#sleepingOwlActionsStore").find('option:selected').val();
+        if (action_value == 0) {
+            Swal.fire({
+                title: trans('lang.table.no-action'),
+                text: trans('lang.select.nothing'),
+                icon: 'error',
+                timer: 5000
+            })
+            return false;
         }
 
+        let $checkboxes = $('.adminCheckboxRow').filter(':checked');
+        if ($checkboxes.length == 0) {
+            Swal.fire({
+                title: trans('lang.select.nothing'),
+                text: trans('lang.select.no_items'),
+                icon: 'error',
+                timer: 5000
+            })
+
+            return false;
+        }
 
         Admin.Messages.confirm(trans('lang.table.action-confirm'), null, self).then(result => {
             if (result.value) {
-
                 let $datatable_wrapper = $(self).parents('.card').find('.dataTables_wrapper'),
                     $checkboxes = $datatable_wrapper.find('.adminCheckboxRow').filter(':checked'),
                     $selectActions = $(self).find(".sleepingOwlActionsStore");
@@ -44,7 +39,6 @@ Admin.Modules.register('display.actions', () => {
                 // console.log($selectActions);
 
                 let data = $checkboxes.serialize();
-
                 let settings = {
                     type: $selectActions.find('option:selected').data('method'),
                     url: $selectActions.val(),
@@ -54,27 +48,31 @@ Admin.Modules.register('display.actions', () => {
 
                 Admin.Events.fire("datatables::actions::submitting", settings);
 
-            $.ajax(settings).done(function (msg) {
-                if (msg.hasOwnProperty('text')) {
-                    Swal.fire({
-                      title: msg.text,
-                      text: msg.message,
-                      icon: msg.type,
-                      timer: 5000
-                    })
-                }
-                if (msg.hasOwnProperty('__callback')) {
-                    let callback_name = msg.__callback;
-                    if (typeof window[callback_name] == 'function') {
-                        window[callback_name]($datatable_wrapper, $checkboxes, $selectActions);
+                $.ajax(settings).done(function (msg) {
+                    if (msg.hasOwnProperty('text')) {
+                        Swal.fire({
+                            title: msg.text,
+                            text: msg.message,
+                            icon: msg.type,
+                            timer: 5000
+                        })
                     }
-                }
-            });
+                    if (msg.hasOwnProperty('__callback')) {
+                        let callback_name = msg.__callback;
+                        if (typeof window[callback_name] == 'function') {
+                            window[callback_name]($datatable_wrapper, $checkboxes, $selectActions, msg);
+                        }
+                    }
+                    if (msg.hasOwnProperty('__redirect')) {
+                        location.href = msg.__redirect;
+                    }
+                });
 
                 Admin.Events.fire("datatables::actions::submitted", self);
-                //reload datatables
+
+                // Reload datatables
                 $('.datatables').DataTable().draw();
-            }else
+            } else
                 Admin.Events.fire("datatables::actions::cancel", self);
         });
 
