1f9053290f8c68b5851e7143fd19c1cbef7f3f6a
Heiswald <Vladysalv.topchii@gmail.com>
2024-07-15 16:18:22 +0300

    fix styleci

--- src/Traits/SqlQueryOperators.php
+++ src/Traits/SqlQueryOperators.php
@@ -2,11 +2,11 @@
 
 namespace SleepingOwl\Admin\Traits;
 
+use DB;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Query\Grammars\PostgresGrammar;
 use Illuminate\Support\Arr;
 use SleepingOwl\Admin\Exceptions\FilterOperatorException;
-use DB;
 
 trait SqlQueryOperators
 {
