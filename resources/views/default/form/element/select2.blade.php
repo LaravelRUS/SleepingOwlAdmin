fa2a4968690479cbae86fa005e9ed88f27364f5d
Daan <daan@ukr.net>
2025-07-11 05:25:22 +0200

    Fix select2 blade

--- resources/views/default/form/element/select2.blade.php
+++ resources/views/default/form/element/select2.blade.php
@@ -8,7 +8,21 @@
     </label>
 
     <div>
-        {!! Form::select($name, collect($options)->pluck('text', 'id'), $value, $attributes_array) !!}
+{{--        {!! Form::select($name, collect($options)->pluck('text', 'id'), $value, $attributes_array) !!}--}}
+
+        @php
+            $attrs = collect($attributes_array ?? [])->map(function($v, $k) {
+                return $k . '="' . e($v) . '"';
+            })->implode(' ');
+        @endphp
+
+        <select name="{{ $name }}" {{ $attrs }}>
+            @foreach($options as $option)
+                <option value="{{ $option['id'] }}" @selected($option['id'] == $value)>
+                    {{ $option['text'] }}
+                </option>
+            @endforeach
+        </select>
     </div>
 
     @include(AdminTemplate::getViewPath('form.element.partials.helptext'))
