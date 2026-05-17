922af8a48bda5818ca5ad36fe02dad6d70d8f2a2
Daan <daan@ukr.net>
2026-05-17 18:50:11 +0200

    Fix all closure

--- src/Navigation/Page.php
+++ src/Navigation/Page.php
@@ -5,12 +5,72 @@ namespace SleepingOwl\Admin\Navigation;
 use Closure;
 use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Illuminate\View\View;
 use SleepingOwl\Admin\Contracts\ModelConfigurationInterface;
+use SleepingOwl\Admin\Contracts\Navigation\BadgeInterface;
 use SleepingOwl\Admin\Contracts\Navigation\PageInterface;
+use SleepingOwl\Admin\Support\HtmlAttributes;
 
-class Page extends PageBase implements PageInterface
+class Page extends NavigationBase implements PageInterface
 {
+    use HtmlAttributes;
+
+    /**
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * @var string|Closure
+     */
+    protected $title;
+
+    /**
+     * Menu item icon.
+     *
+     * @var string|Closure
+     */
+    protected $icon;
+
+    /**
+     * Menu item url.
+     *
+     * @var string|Closure|UrlGenerator|null
+     */
+    protected $url;
+
+    /**
+     * @var BadgeInterface[]|Collection
+     */
+    protected $badges;
+
+    /**
+     * @var int|Closure
+     */
+    protected $priority = 100;
+
+    /**
+     * @var bool|Closure
+     */
+    protected $active = false;
+
+    /**
+     * @var PageInterface
+     */
+    protected $parent;
+
+    /**
+     * @var array
+     */
+    protected $options = [];
+
+    /**
+     * @var array
+     */
+    protected $aliases = [];
+
     /**
      * Menu item related model class.
      *
@@ -28,24 +88,242 @@ class Page extends PageBase implements PageInterface
     /**
      * Menu item target attribute.
      *
-     * @var string
+     * @var string|Closure
      */
     protected $target;
 
     /**
-     * @param  string|null  $modelClass
+     * Type navigation `divider` or `label`.
+     *
+     * @var string|null
      */
-    public function __construct($modelClass = null)
+    protected $type = null;
+
+    /**
+     * Page constructor.
+     *
+     * @param  string|Closure|null  $title
+     * @param  string|Closure|null  $url
+     * @param  string|null  $id
+     * @param  int|Closure|null  $priority
+     * @param  string|Closure|null  $icon
+     */
+    public function __construct($title = null, $url = null, ?string $id = null, $priority = 100, $icon = null)
     {
         parent::__construct();
 
-        $this->setModel($modelClass);
+        $this->badges = new Collection();
 
-        if ($this->hasModel()) {
-            if ($this->getModelConfiguration()->getIcon()) {
-                $this->setIcon($this->getModelConfiguration()->getIcon());
+        if (is_string($title) && class_exists($title)) {
+            $this->setModel($title);
+
+            if ($this->hasModel()) {
+                if ($this->getModelConfiguration()->getIcon()) {
+                    $this->setIcon($this->getModelConfiguration()->getIcon());
+                }
             }
+        } else {
+            $this->title = $title;
         }
+
+        if (! is_null($url)) {
+            $this->setUrl($url);
+        }
+
+        if (! is_null($id)) {
+            $this->setId($id);
+        }
+
+        if (! is_null($priority)) {
+            $this->setPriority($priority);
+        }
+
+        if (! is_null($icon)) {
+            $this->setIcon($icon);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getAliases()
+    {
+        return $this->aliases;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasAliases()
+    {
+        return count($this->aliases) > 0;
+    }
+
+    /**
+     * @param  string|array  $aliases
+     * @return $this
+     */
+    public function addAlias($aliases)
+    {
+        if (! is_array($aliases)) {
+            $aliases = func_get_args();
+        }
+
+        foreach ($aliases as $alias) {
+            $this->aliases[] = $alias;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param  string|array|PageInterface|null  $page
+     * @return PageInterface|null
+     */
+    public function addPage($page = null)
+    {
+        if ($page = parent::addPage($page)) {
+            $page->setParent($this);
+        }
+
+        return $page;
+    }
+
+    /**
+     * @param  Closure  $callback
+     * @return $this
+     */
+    public function setPages(Closure $callback)
+    {
+        call_user_func($callback, $this);
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        if (is_null($this->id)) {
+            if ($this->hasModel()) {
+                return $this->model;
+            }
+
+            return md5(implode('/', $this->getPath()));
+        }
+
+        return $this->id;
+    }
+
+    /**
+     * @param  string  $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        if (is_null($this->title) && $this->hasModel()) {
+            return $this->getModelConfiguration()->getTitle();
+        }
+
+        if ($this->title instanceof Closure) {
+            return call_user_func($this->title, $this);
+        }
+
+        return $this->title;
+    }
+
+    /**
+     * @param  string|Closure  $title
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIcon()
+    {
+        $icon = $this->icon;
+
+        if ($icon instanceof Closure) {
+            $icon = call_user_func($icon, $this);
+        }
+
+        if (is_null($icon) || empty($icon)) {
+            return null;
+        }
+
+        if (strpos($icon, '<i') !== false) {
+            return $icon;
+        }
+
+        return "<i class=\"{$icon}\"></i>";
+    }
+
+    /**
+     * @param  string|Closure  $icon
+     * @return $this
+     */
+    public function setIcon($icon)
+    {
+        $this->icon = $icon;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUrl()
+    {
+        if (is_null($this->url) && $this->hasModel()) {
+            return $this->getModelConfiguration()->getDisplayUrl();
+        }
+
+        if ($this->url instanceof Closure) {
+            return $this->fixUrl(call_user_func($this->url, $this));
+        }
+
+        if ($this->url instanceof UrlGenerator) {
+            return $this->fixUrl($this->url->full());
+        }
+
+        if (is_string($this->url)) {
+            if (strpos($this->url, '://') !== false || strpos($this->url, '//') === 0) {
+                return $this->fixUrl($this->url);
+            }
+
+            return $this->fixUrl(url($this->url));
+        }
+
+        return $this->url;
+    }
+
+    /**
+     * @param  string|Closure  $url
+     * @return $this
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
     }
 
     /**
@@ -88,37 +366,14 @@ class Page extends PageBase implements PageInterface
     }
 
     /**
-     * @return string
-     */
-    public function getId()
-    {
-        if (is_null($this->id) && $this->hasModel()) {
-            return $this->model;
-        }
-
-        return parent::getId();
-    }
-
-    /**
-     * @return string
-     */
-    public function getTitle()
-    {
-        if (is_null($this->title) && $this->hasModel()) {
-            return $this->getModelConfiguration()->getTitle();
-        }
-
-        return parent::getTitle();
-    }
-
-    /**
-     * Set Target.
-     *
-     * @param  $target
+     * @param  string|Closure  $target
+     * @return $this
      */
     public function setTarget($target)
     {
         $this->target = $target;
+
+        return $this;
     }
 
     /**
@@ -126,19 +381,189 @@ class Page extends PageBase implements PageInterface
      */
     public function getTarget()
     {
+        if ($this->target instanceof Closure) {
+            return call_user_func($this->target, $this);
+        }
+
         return $this->target;
     }
 
     /**
-     * @return string
+     * @return array
      */
-    public function getUrl()
+    public function getPath()
     {
-        if (is_null($this->url) && $this->hasModel()) {
-            return $this->getModelConfiguration()->getDisplayUrl();
+        $data = [
+            $this->getTitle(),
+        ];
+
+        $page = $this;
+
+        while (! is_null($page = $page->getParent())) {
+            $data[] = $page->getTitle();
         }
 
-        return $this->getParentUrl();
+        return array_reverse($data);
+    }
+
+    /**
+     * @return array
+     */
+    public function getPathArray()
+    {
+        $data = [
+            $this->toArray(),
+        ];
+
+        $page = $this;
+
+        while (! is_null($page = $page->getParent())) {
+            $data[] = $page->toArray();
+        }
+
+        return array_reverse($data);
+    }
+
+    /**
+     * @return BadgeInterface[]|Collection
+     */
+    public function getBadges()
+    {
+        return $this->badges;
+    }
+
+    /**
+     * @param  BadgeInterface  $badge
+     * @return $this
+     */
+    public function setBadge(BadgeInterface $badge)
+    {
+        $this->badges->push($badge);
+
+        return $this;
+    }
+
+    /**
+     * @param Closure|string $value
+     * @param array|null $attributes
+     * @return $this
+     */
+    public function addBadge($value, ?array $attributes = null)
+    {
+        $this->setBadge(
+            $badge = app(BadgeInterface::class)
+        );
+
+        $badge->setValue($value);
+
+        if (is_array($attributes)) {
+            $badge->setHtmlAttributes($attributes);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPriority()
+    {
+        $priority = $this->priority;
+
+        if ($priority instanceof Closure) {
+            $priority = call_user_func($priority, $this);
+        }
+
+        return (int) $priority;
+    }
+
+    /**
+     * @param  int|Closure  $priority
+     * @return $this
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive()
+    {
+        if ($this->active instanceof Closure) {
+            return (bool) call_user_func($this->active, $this);
+        }
+
+        return $this->active;
+    }
+
+    /**
+     * @param bool|Closure $active
+     * @return $this
+     */
+    public function setIsActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setActive()
+    {
+        $this->active = true;
+
+        if (! is_null($this->getParent())) {
+            $this->getParent()->setActive();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return PageInterface
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * @param PageInterface $page
+     */
+    protected function setParent(PageInterface $page)
+    {
+        $this->parent = $page;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLevel()
+    {
+        return count($this->getPath()) - 1;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isChild()
+    {
+        return ! is_null($this->getParent());
+    }
+
+    /**
+     * @param  PageInterface  $page
+     * @return bool
+     */
+    public function isChildOf(PageInterface $page)
+    {
+        return $this->isChild() and $this->getParent() === $page;
     }
 
     /**
@@ -152,14 +577,61 @@ class Page extends PageBase implements PageInterface
                     return $this->getModelConfiguration()->isDisplayable();
                 };
             }
+
+            if (! is_null($parent = $this->getParent())) {
+                return $parent->getAccessLogic();
+            }
         }
 
         return parent::getAccessLogic();
     }
 
     /**
+     * @return bool|Closure
+     */
+    public function checkAccess()
+    {
+        $accessLogic = $this->getAccessLogic();
+
+        if (is_callable($accessLogic)) {
+            return $accessLogic($this);
+        }
+
+        return $accessLogic;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        if ($this->isActive() and ! $this->hasClassProperty($class = config('navigation.class.active', 'active'))) {
+            $this->setHtmlAttribute('class', $class);
+        }
+
+        if ($this->hasChild() and ! $this->hasClassProperty($class = config('navigation.class.has_child', 'has-child'))) {
+            $this->setHtmlAttribute('class', $class);
+        }
+
+        return parent::toArray() + [
+            'hasChild' => $this->hasChild(),
+            'id' => $this->getId(),
+            'title' => $this->getTitle(),
+            'icon' => $this->getIcon(),
+            'priority' => $this->getPriority(),
+            'url' => $this->getUrl(),
+            'path' => $this->getPath(),
+            'isActive' => $this->isActive(),
+            'attributes' => $this->htmlAttributesToString(),
+            'badges' => $this->getBadges()->sortBy(function (BadgeInterface $badge) {
+                return $badge->getPriority();
+            }),
+        ];
+    }
+
+    /**
      * @param  string|null  $view
-     * @return Factory|View
+     * @return Factory|View|string
      */
     public function render($view = null)
     {
@@ -222,13 +694,6 @@ class Page extends PageBase implements PageInterface
     }
 
     /**
-     * Type navigation `divider` or `label`.
-     *
-     * @var string|null
-     */
-    protected $type = null;
-
-    /**
      * @param  string  $type
      * @return $this
      */
@@ -240,30 +705,19 @@ class Page extends PageBase implements PageInterface
     }
 
     /**
-     * @return string|null
+     * @param  string  $url
+     * @return string
      */
-    public function getParentUrl(): ?string
+    protected function fixUrl($url)
     {
-        if (is_null($this->url)) {
+        if (is_null($url)) {
             return null;
         }
 
-        if ($this->url instanceof Closure) {
-            return call_user_func($this->url, $this);
+        if (Str::startsWith(config('app.url'), 'https://') && Str::startsWith($url, 'http://')) {
+            return str_replace('http://', 'https://', $url);
         }
 
-        if (strpos($this->url, '://') !== false) {
-            return $this->url;
-        }
-
-        if (is_string($this->url)) {
-            return url($this->url);
-        }
-
-        if ($this->url instanceof UrlGenerator) {
-            return $this->url->full();
-        }
-
-        return $this->url;
+        return $url;
     }
 }
--- tests/Admin/Navigation/PageTest.php
+++ tests/Admin/Navigation/PageTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use SleepingOwl\Admin\Navigation\Page;
+use Illuminate\Support\Facades\Config;
 
 class PageTest extends TestCase
 {
@@ -37,5 +38,92 @@ class PageTest extends TestCase
         $page->setUrl('http://external.com');
 
         $this->assertEquals('http://external.com', $page->getUrl());
+    }
+
+    public function test_title_resolution_closure()
+    {
+        $page = $this->getPage();
+        $page->setTitle(function () {
+            return 'Closure Title';
+        });
+
+        $this->assertEquals('Closure Title', $page->getTitle());
+    }
+
+    public function test_icon_resolution_closure()
+    {
+        $page = $this->getPage();
+        $page->setIcon(function () {
+            return 'fa-user';
+        });
+
+        $this->assertEquals('<i class="fa-user"></i>', $page->getIcon());
+    }
+
+    public function test_priority_resolution_closure()
+    {
+        $page = $this->getPage();
+        $page->setPriority(function () {
+            return 500;
+        });
+
+        $this->assertEquals(500, $page->getPriority());
+    }
+
+    public function test_target_resolution_closure()
+    {
+        $page = $this->getPage();
+        $page->setTarget(function () {
+            return '_blank';
+        });
+
+        $this->assertEquals('_blank', $page->getTarget());
+    }
+
+    public function test_active_resolution_closure()
+    {
+        $page = $this->getPage();
+        $page->setIsActive(function () {
+            return true;
+        });
+
+        $this->assertTrue($page->isActive());
+
+        $page->setIsActive(function () {
+            return false;
+        });
+
+        $this->assertFalse($page->isActive());
+    }
+
+    public function test_fixes_http_to_https_when_app_url_is_https()
+    {
+        Config::set('app.url', 'https://domain.com');
+        
+        $page = new Page();
+        $page->setUrl('http://domain.com/admin/test');
+
+        $this->assertEquals('https://domain.com/admin/test', $page->getUrl());
+    }
+
+    public function test_does_not_fix_http_when_app_url_is_http()
+    {
+        Config::set('app.url', 'http://domain.com');
+        
+        $page = new Page();
+        $page->setUrl('http://domain.com/admin/test');
+
+        $this->assertEquals('http://domain.com/admin/test', $page->getUrl());
+    }
+
+    public function test_fixes_relative_url_when_app_url_is_https()
+    {
+        Config::set('app.url', 'https://domain.com');
+        
+        $page = new Page();
+        $page->setUrl('admin/test');
+
+        $url = $page->getUrl();
+        $this->assertStringStartsWith('https://', $url);
     }
 }
