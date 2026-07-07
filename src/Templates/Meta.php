e4cdf8a7cda423ce01acc4de9d739da31bb53a92
i p <spamarmy@yandex.ru>
2026-07-07 12:54:10 +0500

    Fix Meta::putGlobalVar() missing implementation

--- src/Templates/Meta.php
+++ src/Templates/Meta.php
@@ -6,4 +6,8 @@ use SleepingOwl\Admin\Contracts\Template\MetaInterface;
 
 class Meta extends \KodiCMS\Assets\Meta implements MetaInterface
 {
+    public function putGlobalVar($var, $value)
+    {
+        return $this->assets()->putGlobalVar($var, $value);
+    }
 }
