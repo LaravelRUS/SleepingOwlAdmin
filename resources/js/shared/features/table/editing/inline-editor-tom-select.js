07cdcaff1b36e99ba91b721f178097852e85e3f2
Daan <daan@ukr.net>
2026-09-10 05:25:48 +0200

    Css update 0008

---
+++ resources/js/shared/features/table/editing/inline-editor-tom-select.js
@@ -0,0 +1,55 @@
+import TomSelect from 'tom-select'
+
+const NATIVE_SELECT_SELECTOR = '[data-inline-editor-select-native]'
+
+export function createInlineEditorTomSelect(element, config, createTomSelect = createTomSelectInstance) {
+    const select = requiredSelect(element)
+    select.value = config.value
+
+    if (config.mode !== 'popup') return createNativeSelectControl(select)
+
+    const tomSelect = createTomSelect(select, {
+        allowEmptyOption: true,
+        closeAfterSelect: true,
+        create: false,
+        maxItems: 1,
+        maxOptions: null,
+        plugins: [],
+    })
+
+    return {
+        clear: () => tomSelect.clear(),
+        destroy: () => tomSelect.destroy(),
+        focusElement: () => tomSelect.focus(),
+        read: () => selectValue(tomSelect.getValue()),
+        setDisabled: (disabled) => tomSelect[disabled ? 'disable' : 'enable'](),
+    }
+}
+
+function createTomSelectInstance(select, settings) {
+    return new TomSelect(select, settings)
+}
+
+function createNativeSelectControl(select) {
+    return {
+        clear: () => {
+            select.value = ''
+            select.dispatchEvent(new globalThis.Event('change', { bubbles: true }))
+        },
+        focusElement: select,
+        read: () => select.value,
+    }
+}
+
+function requiredSelect(element) {
+    const select = element.querySelector(NATIVE_SELECT_SELECTOR)
+    if (!select) throw new TypeError('Inline editor select template requires a native select.')
+
+    return select
+}
+
+function selectValue(value) {
+    if (Array.isArray(value)) return value[0] ?? ''
+
+    return String(value ?? '')
+}
